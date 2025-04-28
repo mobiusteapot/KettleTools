51ba35783e3f1ecf109f0358b7db6d49d7679129
ezra <ez9002@gmail.com>
2025-04-27 20:17:26 -0400

    Added Unity 6 support
    - Fixes change in how TextMeshPro is provided to the engine

--- KettleToolsProject/Packages/com.mobtp.kt.core/Runtime/Component/CanvasReadmeDisplay.cs
+++ KettleToolsProject/Packages/com.mobtp.kt.core/Runtime/Component/CanvasReadmeDisplay.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 // Currently assuming UI is always included
 using UnityEngine.UI;
-#if PACKAGE_TEXTMESHPRO
+#if PACKAGE_TEXTMESHPRO || UNITY_600_0_OR_NEWER
 using TMPro;
 #endif
 using System.Collections.Generic;
@@ -15,8 +15,7 @@ namespace Mobtp.KT.Core.Docs {
     [RequireComponent(typeof(CanvasRenderer), typeof(RectTransform))]
     public class CanvasReadmeDisplay : MonoBehaviour {
         // All population should be done in editor, no runtime access of any kind should occur
-        #if UNITY_EDITOR
-        #if PACKAGE_TEXTMESHPRO
+#if UNITY_EDITOR && (PACKAGE_TEXTMESHPRO || UNITY_600_0_OR_NEWER)
         public CanvasReadmeSettings Settings;
         [SerializeField] private Readme readme;
 
@@ -142,7 +141,6 @@ namespace Mobtp.KT.Core.Docs {
         rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, width);
         rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, height);
     }
-    #endif
     #endif
     }
 }
